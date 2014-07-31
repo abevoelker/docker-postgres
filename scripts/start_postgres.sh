af905f08361bb42d3ba49133174111094e382df7
Abe Voelker <abe@abevoelker.com>
2014-07-31 15:34:52 -0500

    Remove heredoc from Postgres start script. Fixes #4

--- scripts/start_postgres.sh
+++ scripts/start_postgres.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 chown -R root:postgres /etc/wal-e.d
 chmod -R 750           /etc/wal-e.d
-su postgres -- <<EOF
-  /usr/lib/postgresql/9.1/bin/postgres \
-    -D /var/lib/postgresql/9.1/main \
-    -c config_file=/etc/postgresql/9.1/main/postgresql.conf
-EOF
+exec su postgres -c "/usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf"
