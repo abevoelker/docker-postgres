9fccb3dc3ceab40414270b9475526c3a4e7fee04
Abe Voelker <abe@abevoelker.com>
2014-07-31 00:35:01 -0500

    Switch base image to phusion/baseimage
    Useful to easily get a sane install of cron, syslog, and runit

--- scripts/start_postgres.sh
+++ scripts/start_postgres.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 chown -R root:postgres /etc/wal-e.d
 chmod -R 750           /etc/wal-e.d
-su postgres -- <<EOF
+exec su postgres -- <<EOF
   /usr/lib/postgresql/9.3/bin/postgres \
     -D /var/lib/postgresql/9.3/main \
     -c config_file=/etc/postgresql/9.3/main/postgresql.conf
