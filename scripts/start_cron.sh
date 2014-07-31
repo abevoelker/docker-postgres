1814560751cbd007db18b0d97b1aad737045007d
Abe Voelker <abe@abevoelker.com>
2014-07-31 00:35:01 -0500

    Switch base image to phusion/baseimage
    Useful to easily get a sane install of cron, syslog, and runit

--- scripts/start_cron.sh
+++ scripts/start_cron.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-chown -R root:root     /etc/cron.d
-chmod -R 744           /etc/cron.d
+chown -R root:root     /etc/cron.{d,daily,hourly,monthly,weekly}
+chmod -R 755           /etc/cron.{d,daily,hourly,monthly,weekly}
 chown -R root:postgres /etc/wal-e.d
 chmod -R 750           /etc/wal-e.d
-cron -f
+exec cron -f
