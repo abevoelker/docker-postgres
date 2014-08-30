8a2339fe9c0fcd6a934c3179a4df896bcdafc6a5
Abe Voelker <abe@abevoelker.com>
2014-08-30 16:58:02 -0500

    Ensure ownership/permissions on /etc/postgresql/9.3/main

--- scripts/ensure_ownership.sh
+++ scripts/ensure_ownership.sh
@@ -4,3 +4,5 @@ chmod -R 755               /etc/cron.{d,daily,hourly,monthly,weekly}
 chown -R root:postgres     /etc/wal-e.d
 chmod -R 750               /etc/wal-e.d
 chown -R postgres:postgres /var/lib/postgresql/9.3
+chown -R postgres:postgres /etc/postgresql/9.3/main
+chmod -R 600               /etc/postgresql/9.3/main
